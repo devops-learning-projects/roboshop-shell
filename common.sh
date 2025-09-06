29825702f0e2de1ea5deb07668194fd0f6c50208
Mainul Islam <mainulislam6168@gmail.com>
2025-09-06 10:10:32 +0600

    code dry

--- common.sh
+++ common.sh
@@ -1,20 +1,32 @@
-NODEJS() {
-  dnf module disable nodejs -y
-  dnf module enable nodejs:20 -y
-  dnf install nodejs -y
+APP_PREREQ() {
   cp ${component}.service /etc/systemd/system/${component}.service
-
   cartadd roboshop
   rm -rf /app
   mkdir /app
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
   cd /app
   unzip /tmp/${component}.zip
-  cd /app
-  npm install
+}
 
+SYSTEMD() {
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl start ${component}
+}
 
+NODEJS() {
+  dnf module disable nodejs -y
+  dnf module enable nodejs:20 -y
+  dnf install nodejs -y
+
+  APP_PREREQ
+  npm install
+  SYSTEMD
+}
+PYTHON() {
+  dnf install python3 gcc python3-devel -y
+  APP_PREREQ
+  pip3 install -r requirements.txt
+
+  SYSTEMD
 }
