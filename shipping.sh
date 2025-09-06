fe59b7131f0bf8ea5ea2870d0fcdd97489a8d9f3
Mainul Islam <mainulislam6168@gmail.com>
2025-09-06 10:39:19 +0600

    hide password

--- shipping.sh
+++ shipping.sh
@@ -3,6 +3,6 @@ component=shipping
 MAVEN
 
 dnf install mysql -y
-mysql -h mysql-dev.maidevops.fun -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.maidevops.fun -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h mysql-dev.maidevops.fun -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql-dev.maidevops.fun -uroot -p$1 < /app/db/schema.sql
+mysql -h mysql-dev.maidevops.fun -uroot -p$1 < /app/db/app-user.sql
+mysql -h mysql-dev.maidevops.fun -uroot -p$1 < /app/db/master-data.sql
