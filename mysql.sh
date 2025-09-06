51fab588ebbcc205dc90fc451b34b92d67ef0ddf
Mainul Islam <mainulislam6168@gmail.com>
2025-09-06 17:05:21 +0600

    hide password

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,9 @@
+if [ -z "$1 "]; then
+  echo Input Password is Missing
+  exit
+fi
+
 dnf install mysql-server -y
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass $1
